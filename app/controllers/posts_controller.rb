4a335e841cd5bdc288832e746bb8997502616d3e
Mark Bryles <mbryles@gmail.com>
2012-08-11 19:34:11 -0400

    initial commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,75 @@
+class PostsController < ApplicationController
+  
+  http_basic_authenticate_with :name => "dhh", :password => "secret", :except => [:index, :show]
+  
+  def new
+     @post = Post.new
+  end
+  def create  
+    @post = Post.new(params[:post])  
+  
+    respond_to do |format|  
+      if @post.save  
+        format.html { redirect_to(@post, :notice => 'Post created.') }  
+        format.js  
+      else  
+        format.html { render :action => "new" }  
+        format.js  
+      end  
+    end  
+end  
+  def create2
+    @post = Post.new(params[:post])
+    
+    if @post.save
+      #redirect_to :action => :show, :id => @post.id
+        format.html { redirect_to(@post, :notice => 'Post created.') }
+        format.js
+    else
+      #render 'new'
+        format.html { render :action => "new" }
+        format.js
+    end  
+  end
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+  
+  def index
+    @posts = Post.all
+  end
+  
+  def edit
+    @post = Post.find(params[:id])
+  end
+  def update  
+    @post = Post.find(params[:id])
+  
+    respond_to do |format|  
+      if @post.save  
+        format.html { redirect_to(:action => :show, :id => @post.id) }  
+        format.js  
+      else  
+        format.html { render :action => "edit" }  
+        format.js  
+      end  
+    end  
+end  
+  def update2
+    @post = Post.find(params[:id])
+ 
+    if @post.update_attributes(params[:post])
+      redirect_to :action => :show, :id => @post.id
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+ 
+    redirect_to :action => :index
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '9b7b10790c6834c3c6b7fac34b9b7204846039cc2b004adb6ee13ea259fbcbc37287acbb823908020e5c762f6791c97aaef36b5163564405611da96dfe89de7a'
