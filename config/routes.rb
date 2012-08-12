cc7230ef9ff4e1c5ab26bf4ce8a5edd6bfb3e8eb
Mark Bryles <mbryles@gmail.com>
2012-08-11 20:51:43 -0400

    added users resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blog::Application.routes.draw do
+  resources :users
+
   get "welcome/index"
   #get "posts/new"
   #post "posts/create"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120522023157) do
+ActiveRecord::Schema.define(:version => 20120812000919) do
 
   create_table "comments", :force => true do |t|
     t.string   "commenter"
@@ -26,6 +26,15 @@ ActiveRecord::Schema.define(:version => 20120522023157) do
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "text"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "user_id"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.boolean  "is_admin"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
